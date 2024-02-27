ff53edb67a2375c1aa4de7aac611899f9dd2a864
Jens Astrup <jensaiden@gmail.com>
2024-02-26 20:22:08 -0500

    Refactor code into class

--- dist/index.js
+++ dist/index.js
@@ -10,22 +10,48 @@ __nccwpck_require__.r(__webpack_exports__);
 /* harmony import */ var openai__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(9725);
 
 
-
 const github = __nccwpck_require__(5514)
 const core = __nccwpck_require__(6164)
 
-console.log('Running review script...')
 const client = new openai__WEBPACK_IMPORTED_MODULE_0__/* ["default"] */ .ZP({apiKey: process.env.INPUT_OPENAI_API_KEY})
 
+
 class PullRequestReviewer {
-  constructor(diffText) {
-    this.diffText = diffText
+  constructor() {
+    const {context = {}} = github
+    this.context = context
+    this.octokit = github.getOctokit(token)
+  }
+
+  async getDiff() {
+    console.log('Run function started...')
+    const token = core.getInput('GITHUB_TOKEN', {required: true})
+
+
+    console.log('Getting pull request...')
+
+    const {pull_request} = this.context.payload
+
+    if (!pull_request) {
+      core.setFailed('Could not get pull request from context')
+      return
+    }
+
+    const {data: diff} = await this.octokit.rest.pulls.get({
+      owner: this.context.repo.owner,
+      repo: this.context.repo.repo,
+      pull_number: pull_request.number,
+      mediaType: {
+        format: 'diff'
+      }
+    })
+    return diff
   }
 
   async createThread() {
     const messages = [{
       role: 'user',
-      content: this.diffText
+      content: this.getDiff()
     }]
     return client.beta.threads.create({
         messages: messages
@@ -46,7 +72,7 @@ class PullRequestReviewer {
     return client.beta.threads.runs.retrieve(threadId, runId)
   }
 
-  async review() {
+  async getReview() {
     let {run, threadId} = await this.runThread()
     while (!['cancelled', 'failed', 'completed', 'expired'].includes(run.status)) {
       run = await this.retrieveThread(run.id, threadId)
@@ -58,53 +84,30 @@ class PullRequestReviewer {
     return messages.data[0].content[0].text.value
   }
 
+  async commentOnPullRequest(comment) {
+    const prNumber = this.context.payload.number
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
+    await this.octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: prNumber,
+      body: comment
+    })
+  }
+
+  async review() {
+    const review = await this.getReview()
+    await this.commentOnPullRequest(review)
+    return review
+
+}
 }
 
 
 async function run() {
   try {
-    console.log('Run function started...')
-    const token = core.getInput('GITHUB_TOKEN', {required: true})
-    const octokit = github.getOctokit(token)
-
-    console.log('Getting pull request...')
-    const {context = {}} = github
-    console.log('Context:', context)
-    const {pull_request} = context.payload
-
-    if (!pull_request) {
-      core.setFailed('Could not get pull request from context')
-      return
-    }
-
-    const {data: diff} = await octokit.rest.pulls.get({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      pull_number: pull_request.number,
-      mediaType: {
-        format: 'diff'
-      }
-    });
-
-
-    console.log('Running review script...')
-
-    const reviewer = new PullRequestReviewer(diff)
-    const output = await reviewer.review()
-
-    console.log('Review generated successfully.')
-    const prNumber = context.payload.number
-    const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
-    console.log('Owner:', owner)
-    console.log('Repo:', repo)
-    // Post comment to PR
-    await octokit.rest.issues.createComment({
-      owner,
-      repo,
-      issue_number: prNumber,
-      body: output
-    })
-
+    const reviewer = new PullRequestReviewer()
+    await reviewer.review()
     console.log('Review comment posted successfully.')
 
   } catch (error) {
